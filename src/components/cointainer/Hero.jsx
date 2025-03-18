ba3970e8e53b2fbc57dded5bc5de80b895823006
sibiraj10 <sibirajselvam1@gmail.com>
2025-03-18 13:51:21 +0530

    f

--- src/components/cointainer/Hero.jsx
+++ src/components/cointainer/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImg from "../../assets/HeroImg.png";
+import HeroImg from "../../assets/Heroimg.png";
 import  { Link, useNavigate } from 'react-router-dom'
 
 const Hero = () => {
